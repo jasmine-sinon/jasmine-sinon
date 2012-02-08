bc37a3614efea18e64a3da839d0793e103c8d5b6
Augusto Elesbão <augusto.elesbao@netmovies.com.br>
2012-02-08 15:45:46 -0200

    Added threw() spy matcher

--- spec/jasmine-sinon-specs.js
+++ spec/jasmine-sinon-specs.js
@@ -1,75 +1,75 @@
 describe("jasmine-sinon", function() {
-  
+
   describe("spy matchers", function() {
-    
+
     beforeEach(function() {
       this.spy = sinon.spy();
     });
-    
+
     describe("called/toHaveBeenCalled", function() {
-      
+
       it("should not match when spy not called", function() {
         expect(this.spy.called).toBeFalsy();
         expect(this.spy).not.toHaveBeenCalled();
       });
-      
+
       it("should match when spy called once", function() {
         this.spy();
         expect(this.spy.called).toBeTruthy();
         expect(this.spy).toHaveBeenCalled();
       });
-      
+
       it("should match when spy called twice", function() {
         this.spy();
         this.spy();
         expect(this.spy.called).toBeTruthy();
         expect(this.spy).toHaveBeenCalled();
       });
-      
+
     });
-    
+
     describe("calledOnce/toHaveBeenCalledOnce", function() {
-      
+
       it("should not match when spy not called", function() {
         expect(this.spy.calledOnce).toBeFalsy();
         expect(this.spy).not.toHaveBeenCalledOnce();
       });
-      
+
       it("should match when spy called once", function() {
         this.spy();
         expect(this.spy.calledOnce).toBeTruthy();
         expect(this.spy).toHaveBeenCalledOnce();
       });
-      
+
       it("should not match when spy called twice", function() {
         this.spy();
         this.spy();
         expect(this.spy.calledOnce).toBeFalsy();
         expect(this.spy).not.toHaveBeenCalledOnce();
       });
-      
+
     });
-    
+
     describe("calledTwice/toHaveBeenCalledTwice", function() {
-      
+
       it("should not match when spy not called", function() {
         expect(this.spy.calledTwice).toBeFalsy();
         expect(this.spy).not.toHaveBeenCalledTwice();
       });
-      
+
       it("should not match when spy called once", function() {
         this.spy();
         expect(this.spy.calledTwice).toBeFalsy();
         expect(this.spy).not.toHaveBeenCalledTwice();
       });
-      
+
       it("should match when spy called twice", function() {
         this.spy();
         this.spy();
         expect(this.spy.calledTwice).toBeTruthy();
         expect(this.spy).toHaveBeenCalledTwice();
       });
-      
+
       it("should not match when spy called thrice", function() {
         this.spy();
         this.spy();
@@ -77,29 +77,29 @@ describe("jasmine-sinon", function() {
         expect(this.spy.calledTwice).toBeFalsy();
         expect(this.spy).not.toHaveBeenCalledTwice();
       });
-      
+
     });
-    
+
     describe("calledThrice/toHaveBeenCalledThrice", function() {
-      
+
       it("should not match when spy not called", function() {
         expect(this.spy.calledThrice).toBeFalsy();
         expect(this.spy).not.toHaveBeenCalledThrice();
       });
-      
+
       it("should not match when spy called once", function() {
         this.spy();
         expect(this.spy.calledThrice).toBeFalsy();
         expect(this.spy).not.toHaveBeenCalledThrice();
       });
-      
+
       it("should not match when spy called twice", function() {
         this.spy();
         this.spy();
         expect(this.spy.calledThrice).toBeFalsy();
         expect(this.spy).not.toHaveBeenCalledThrice();
       });
-      
+
       it("should match when spy called thrice", function() {
         this.spy();
         this.spy();
@@ -107,7 +107,7 @@ describe("jasmine-sinon", function() {
         expect(this.spy.calledThrice).toBeTruthy();
         expect(this.spy).toHaveBeenCalledThrice();
       });
-      
+
       it("should not match when spy called four times", function() {
         this.spy();
         this.spy();
@@ -116,16 +116,16 @@ describe("jasmine-sinon", function() {
         expect(this.spy.calledThrice).toBeFalsy();
         expect(this.spy).not.toHaveBeenCalledThrice();
       });
-      
+
     });
-    
+
     describe("calledBefore/After - toHaveBeenCalledBefore/After", function() {
-      
+
       beforeEach(function() {
         this.spyA = sinon.spy();
         this.spyB = sinon.spy();
       });
-      
+
       describe("calledBefore / toHaveBeenCalledBefore", function() {
         it("should match when spy a called before spy b", function() {
           this.spyA();
@@ -141,7 +141,7 @@ describe("jasmine-sinon", function() {
           expect(this.spyA).not.toHaveBeenCalledBefore(this.spyB);
         });
       });
-      
+
       describe("calledAfter / toHaveBeenCalledAfter", function() {
         it("should match when spy a called after spy b", function() {
           this.spyA();
@@ -157,11 +157,11 @@ describe("jasmine-sinon", function() {
           expect(this.spyB).not.toHaveBeenCalledAfter(this.spyA);
         });
       });
-      
+
     });
-    
+
     describe("calledOn/toHaveBeenCalledOn", function() {
-      
+
       it("should match when spy called on expected object", function() {
         expect(this.spy.calledOn(this)).toBeFalsy();
         expect(this.spy).not.toHaveBeenCalledOn(this);
@@ -169,99 +169,162 @@ describe("jasmine-sinon", function() {
         expect(this.spy.calledOn(this)).toBeTruthy();
         expect(this.spy).toHaveBeenCalledOn(this);
       });
-      
+
     });
-    
+
     describe("alwaysCalledOn/toHaveAlwaysBeenCalledOn/toHaveBeenAlwaysCalledOn", function() {
-      
+
       it("should match when spy always called on expected object", function() {
         this.spy.call(this);
         this.spy.call(this);
         expect(this.spy.alwaysCalledOn(this)).toBeTruthy();
         expect(this.spy).toHaveBeenAlwaysCalledOn(this);
       });
-      
+
       it("should not match when spy called with other object", function() {
         this.spy.call(this);
         this.spy.call({});
         expect(this.spy.alwaysCalledOn(this)).toBeFalsy();
         expect(this.spy).not.toHaveBeenAlwaysCalledOn(this);
       })
-      
+
     });
-    
+
     describe("calledWith/toHaveBeenCalledWith", function() {
-      
+
       it("should match when spy called with argument", function() {
         this.spy('arg1');
         expect(this.spy.calledWith('arg1')).toBeTruthy();
         expect(this.spy).toHaveBeenCalledWith('arg1');
       });
-      
+
       it("should not match when spy called with different arguments", function() {
         this.spy('arg1');
         expect(this.spy.calledWith('arg2')).toBeFalsy();
         expect(this.spy).not.toHaveBeenCalledWith('arg2');
       });
-      
+
     });
-    
+
     describe("alwaysCalledWith/toHaveBeenAlwaysCalledWith", function() {
-      
+
       it("should match when spy always called with argument", function() {
         this.spy('arg1');
         this.spy('arg1', 'arg2');
         expect(this.spy.alwaysCalledWith('arg1')).toBeTruthy();
         expect(this.spy).toHaveBeenAlwaysCalledWith('arg1');
       });
-      
+
       it("should not match when spy not always called with argument", function() {
         this.spy('arg1');
         this.spy('arg2');
         expect(this.spy.alwaysCalledWith('arg1')).toBeFalsy();
         expect(this.spy).not.toHaveBeenAlwaysCalledWith('arg');
       });
-      
+
     });
-    
+
     describe("calledWithExactly/toHaveBeenCalledWithExactly", function() {
-      
+
       it("should match when spy called with exact argument set", function() {
         this.spy('arg1', 'arg2');
         expect(this.spy.calledWithExactly('arg1', 'arg2')).toBeTruthy();
         expect(this.spy).toHaveBeenCalledWithExactly('arg1', 'arg2');
       });
-      
+
       it("should not match when spy called with different argument set", function() {
         this.spy('arg1', 'arg2', 'arg3');
         expect(this.spy.calledWithExactly('arg1', 'arg2')).toBeFalsy();
         expect(this.spy).not.toHaveBeenCalledWithExactly('arg1', 'arg2');
       });
-      
+
     });
-    
+
     describe("alwaysCalledWithExactly/toHaveBeenAlwaysCalledWithExactly", function() {
-      
+
       it("should match when spy always called with exact argument set", function() {
         this.spy('arg1', 'arg2');
         this.spy('arg1', 'arg2');
         expect(this.spy.alwaysCalledWithExactly('arg1','arg2')).toBeTruthy();
         expect(this.spy).toHaveBeenAlwaysCalledWithExactly('arg1','arg2');
       });
-      
+
       it("should not match when spy called with differing argument set", function() {
         this.spy('arg1', 'arg2');
         this.spy('arg1', 'arg2', 'arg3');
         expect(this.spy.alwaysCalledWithExactly('arg1','arg2')).toBeFalsy();
         expect(this.spy).not.toHaveBeenAlwaysCalledWithExactly('arg1','arg2');
       });
-      
+
     });
-    
-    // TODO exception matchers
-    
+
+    describe("threw/toHaveThrew", function() {
+      beforeEach(function() {
+        this.spy = sinon.spy.create();
+
+        this.spyWithTypeError = sinon.spy.create(function () {
+          throw new TypeError();
+        });
+      });
+
+      it("should match exception thrown by function", function() {
+        var err = new Error();
+
+        var spy = sinon.spy.create(function () {
+          throw err;
+        });
+
+        try {
+          spy();
+        } catch (e) {}
+
+        expect(spy.threw(err)).toBeTruthy();
+        expect(spy).toHaveThrew(err);
+      });
+
+      it("should match when spy threw exception", function() {
+        try {
+          this.spyWithTypeError();
+        } catch(e) {}
+
+        expect(this.spyWithTypeError.threw()).toBeTruthy();
+        expect(this.spyWithTypeError).toHaveThrew();
+      });
+
+      it("should not match when spy did not throw", function() {
+        this.spy();
+        expect(this.spy.threw()).toBeFalsy();
+        expect(this.spy).not.toHaveThrew();
+      });
+
+      it("should match when string type matches", function() {
+        try {
+          this.spyWithTypeError();
+        } catch(e) {}
+
+        expect(this.spyWithTypeError.threw("TypeError")).toBeTruthy();
+        expect(this.spyWithTypeError).toHaveThrew("TypeError");
+      });
+
+      it("should not match when string did not match", function() {
+        try {
+          this.spyWithTypeError();
+        } catch(e) {}
+
+        expect(this.spyWithTypeError.threw("Error")).toBeFalsy();
+        expect(this.spyWithTypeError).not.toHaveThrew("Error");
+      });
+
+      it("should not match when spy did not throw specified error", function() {
+        this.spy();
+        expect(this.spy.threw("TypeError")).toBeFalsy();
+        expect(this.spy).not.toHaveThrew("TypeError");
+      });
+
+    });
+
     describe("returned/toHaveReturned", function() {
-      
+
       it("should match when spy returned value", function() {
         var spy = sinon.spy.create(function() {
           return 1;
@@ -270,7 +333,7 @@ describe("jasmine-sinon", function() {
         expect(spy.returned(1)).toBeTruthy();
         expect(spy).toHaveReturned(1);
       });
-      
+
       it("should match when spy returned value amongst others", function() {
         var values = [1,2,3];
         var spy = sinon.spy.create(function() {
@@ -281,18 +344,18 @@ describe("jasmine-sinon", function() {
         expect(spy.returned(3)).toBeTruthy();
         expect(spy).toHaveReturned(3);
       });
-      
+
       it("should not match when spy did not return value", function() {
         var spy = sinon.spy();
         spy();
         expect(spy.returned(1)).toBeFalsy();
         expect(spy).not.toHaveReturned(1);
       });
-      
+
     });
-    
+
     describe("alwaysReturned/toHaveAlwaysReturned", function() {
-      
+
       it("should match when spy always return value", function() {
         var spy = sinon.spy.create(function() {
           return 1;
@@ -302,7 +365,7 @@ describe("jasmine-sinon", function() {
         expect(spy.alwaysReturned(1)).toBeTruthy();
         expect(spy).toHaveAlwaysReturned(1);
       });
-      
+
       it("should not match when spy did not always return value", function() {
         var values = [1,2,3];
         var spy = sinon.spy.create(function() {
@@ -313,21 +376,21 @@ describe("jasmine-sinon", function() {
         expect(spy.alwaysReturned(3)).toBeFalsy();
         expect(spy).not.toHaveAlwaysReturned(3);
       });
-      
+
       it("should not match when spy did not return value", function() {
         var spy = sinon.spy();
         spy();
         expect(spy.alwaysReturned(1)).toBeFalsy();
         expect(spy).not.toHaveAlwaysReturned(1);
       });
-      
+
     });
 
-    
+
   });
-  
+
   describe("existing method spy matchers", function() {
-    
+
     beforeEach(function() {
       this.methodVal = "no";
       this.api = {
@@ -337,7 +400,7 @@ describe("jasmine-sinon", function() {
       }
       this.spy = sinon.spy(this.api, "myMethod");
     });
-    
+
     describe("boolean matcher example", function() {
       it("should retain spy's original functionality", function() {
         expect(this.methodVal).toEqual("no");
@@ -347,7 +410,7 @@ describe("jasmine-sinon", function() {
         expect(this.spy).toHaveBeenCalled();
       });
     });
-    
+
     describe("method matcher example", function() {
       it("should retain spy's original functionality", function() {
         expect(this.methodVal).toEqual("no");
@@ -357,9 +420,9 @@ describe("jasmine-sinon", function() {
         expect(this.spy).toHaveBeenCalledOn(this);
       });
     });
-        
+
   });
-  
+
   describe("individual spy calls", function() {
 
     beforeEach(function() {
@@ -401,24 +464,24 @@ describe("jasmine-sinon", function() {
       });
 
     });
-    
+
     describe("calledWithExactly/toHaveBeenCalledWithExactly", function() {
-      
+
       it("should return true if received provided arguments and no others", function() {
         this.spy('foo','bar');
         var spyCall = this.spy.getCall(0);
         expect(spyCall.calledWithExactly('foo','bar')).toBeTruthy();
         expect(spyCall).toHaveBeenCalledWithExactly('foo','bar');
       });
-      
+
     });
-    
+
     // TODO individual spy call exception matchers
 
   });
-  
+
   describe("test stubs", function() {
-    
+
     beforeEach(function() {
       this.anonStub = sinon.stub();
       this.api = {
@@ -428,9 +491,9 @@ describe("jasmine-sinon", function() {
       }
       this.methodStub = sinon.stub(this.api,'foo');
     });
-    
+
     describe("inherit spy matchers", function() {
-      
+
       describe("boolean matcher examples", function() {
 
         it("should return true when anonymous stub was called", function() {
@@ -438,7 +501,7 @@ describe("jasmine-sinon", function() {
           expect(this.anonStub.called).toBeTruthy();
           expect(this.anonStub).toHaveBeenCalled();
         });
-        
+
         it("should return true when method stub was called", function() {
           this.api.foo();
           expect(this.methodStub.called).toBeTruthy();
@@ -451,27 +514,27 @@ describe("jasmine-sinon", function() {
           expect(this.methodStub.called).toBeFalsy();
           expect(this.methodStub).not.toHaveBeenCalled();
         });
-        
+
       });
-      
+
       describe("method matcher examples", function() {
-        
+
         it("should return true when anonymous stub called with scope", function() {
           this.anonStub.call(this);
           expect(this.anonStub.calledOn(this)).toBeTruthy();
           expect(this.anonStub).toHaveBeenCalledOn(this);
         });
-        
+
         it("should return true when method stub was called with scope", function() {
           this.api.foo.call(this);
           expect(this.methodStub.calledOn(this)).toBeTruthy();
           expect(this.methodStub).toHaveBeenCalledOn(this);
         });
-        
+
       });
-      
+
     });
-    
+
   });
-  
+
 });
