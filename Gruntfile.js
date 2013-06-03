e650a0d13a4b7d354a248d441dfc3060a7c03847
James Newbery <james@tinnedfruit.com>
2013-06-03 14:36:11 +0100

    Grunt karma test runner task

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,18 +3,30 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-
     jshint: {
       options: {
         jshintrc: '.jshintrc'
       },
       all: ['Gruntfile.js', 'lib/jasmine-sinon.js']
-    }
+    },
 
+    karma: {
+      options: {
+        configFile: 'karma.conf.js'
+      },
+      dev: {
+        reporters: 'dots'
+      },
+      ci: {
+        singleRun: true
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-karma-0.9.1');
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('test', ['jshint', 'karma:ci']);
+  grunt.registerTask('default', 'test');
 
 };
